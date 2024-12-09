8cd97f328d2a87c675495c1f366a33482ec50ff2
ziqiyang6 <ziqiyang91@gmail.com>
2024-12-08 22:50:53 -0500

    Updated codes and new files and data

--- schema.sql
+++ schema.sql
@@ -5,11 +5,12 @@ CREATE TABLE if NOT EXISTS yellowcab
     dropoff_time        TIMESTAMP   NOT NULL,
     passenger_num       INTEGER     NOT NULL,
     trip_distance       FLOAT       NOT NULL,
+    fare_amount         FLOAT       NOT NULL,
     latitude_dropoff    FLOAT       NOT NULL,   
     longitude_dropoff   FLOAT       NOT NULL, 
     latitude_pickup     FLOAT       NOT NULL, 
     longitude_pickup    FLOAT       NOT NULL,
-    UNIQUE(pickup_time, dropoff_time, passenger_num, trip_distance, latitude_dropoff,
+    UNIQUE(pickup_time, dropoff_time, passenger_num, trip_distance, fare_amount, latitude_dropoff,
     longitude_dropoff, latitude_pickup, longitude_pickup )
 );
 
@@ -20,11 +21,12 @@ CREATE TABLE if NOT EXISTS uber
     dropoff_time        TIMESTAMP   NOT NULL,
     trip_time           FLOAT     NOT NULL,
     trip_distance       FLOAT       NOT NULL,
+    fare_amount         FLOAT       NOT NULL,
     latitude_dropoff    FLOAT       NOT NULL,   
     longitude_dropoff   FLOAT       NOT NULL, 
     latitude_pickup     FLOAT       NOT NULL, 
     longitude_pickup    FLOAT       NOT NULL,
-    UNIQUE(pickup_time, dropoff_time, trip_time, trip_distance, latitude_dropoff,
+    UNIQUE(pickup_time, dropoff_time, trip_time, trip_distance, fare_amount, latitude_dropoff,
     longitude_dropoff, latitude_pickup, longitude_pickup )
 );
 
@@ -48,4 +50,13 @@ CREATE TABLE if NOT EXISTS daily_weather
     weather             VARCHAR     NOT NULL,
     snow                FLOAT       NOT NULL,
     UNIQUE(date, temperature, wind_speed, precipitation, weather, snow)
+);
+
+CREATE TABLE if NOT EXISTS weather_sun
+(
+    id                  INTEGER     PRIMARY KEY,
+    date                TIMESTAMP   NOT NULL,
+    sunrise             INTEGER     NOT NULL,
+    sunset              INTEGER       NOT NULL,
+    UNIQUE(date, sunrise, sunset)
 );
